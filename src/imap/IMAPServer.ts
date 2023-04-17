f9ff4312f964cb8b5c1ffb14aa275e620802e698
Cfp <chaosflo44plus@gmail.com>
2023-04-17 20:03:45 +0200

    More

--- src/imap/IMAPServer.ts
+++ src/imap/IMAPServer.ts
@@ -53,11 +53,20 @@ export default class IMAPServer {
           sock.write(tag + " NO [NONEXISTENT] SELECT failed\r")
         }
       } else if(rest.startsWith("fetch")) {
-        const id = rest.split(" ")[1]
-        const mail = await user.$get("mails", { where: { id: id } })
-        sock.write("* 1 FETCH (BODY[] {" + mail.content.length + "}\r")
-        sock.write(mail.content + ")\r")
-        sock.write(tag + " OK FETCH completed\r")
+        // fetch 1 body[...]
+        const id = rest.split(" ")[1] // 1
+        const body = rest.split(" ")[2] // body[...]
+        const mails = await user.$get("mails")
+        const mail = mails[parseInt(id) - 1]
+        if(!mail) {
+          sock.write(tag + " NO [NONEXISTENT] FETCH failed\r")
+          return
+        }
+        if(body == "body[]") {
+          sock.write("* 1 FETCH (BODY[] {" + mail.content.length + "}\r")
+          sock.write(mail.content + ")\r")
+          sock.write(tag + " OK FETCH completed\r")
+        }
       } else if(rest.startsWith("logout")) {
         sock.write("* BYE IMAP4rev1 Server logging out\r")
         sock.write(tag + " OK LOGOUT completed\r")
