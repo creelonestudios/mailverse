8fe6072944ad1fec3c6284af15db354139366a75
Cfp <chaosflo44plus@gmail.com>
2023-04-23 13:03:33 +0200

    return void

--- dist/pop3/POP3Server.js
+++ dist/pop3/POP3Server.js
@@ -25,24 +25,18 @@ export default class POP3Server {
                 sock.write("+OK Capability list follows\r\nUSER\r\n.\r\n");
             }
             else if (msg.startsWith("USER")) { // client gives username
-                if (args.length < 1) {
-                    sock.write("-ERR Invalid username or password\r\n");
-                    return;
-                }
+                if (args.length < 1)
+                    return void sock.write("-ERR Invalid username or password\r\n");
                 username = args[0].trim().toLowerCase();
                 let _user = await User.findOne({ where: { username: username } });
-                if (!_user) {
-                    sock.write("-ERR Invalid username or password\r\n");
-                    return;
-                }
+                if (!_user)
+                    return void sock.write("-ERR Invalid username or password\r\n");
                 user = _user;
                 sock.write("+OK\r\n");
             }
             else if (msg.startsWith("PASS")) { // client gives password
-                if (args.length < 1) {
-                    sock.write("-ERR Invalid username or password\r\n");
-                    return;
-                }
+                if (args.length < 1)
+                    return void sock.write("-ERR Invalid username or password\r\n");
                 const password = args[0].trim();
                 const hash = createHash("sha256");
                 hash.update(password);
@@ -71,28 +65,20 @@ export default class POP3Server {
                 sock.write(".\r\n");
             }
             else if (msg.startsWith("RETR")) {
-                if (args.length < 1) {
-                    sock.write("-ERR No message specified\r\n");
-                    return;
-                }
+                if (args.length < 1)
+                    return void sock.write("-ERR No message specified\r\n");
                 const mail = await user.getMail(parseInt(args[0].trim()));
-                if (!mail) {
-                    sock.write("-ERR No such message\r\n");
-                    return;
-                }
+                if (!mail)
+                    return void sock.write("-ERR No such message\r\n");
                 const content = await readFile("mails/" + mail.content + ".txt", "utf-8");
                 sock.write("+OK\r\n" + content + "\r\n.\r\n");
             }
             else if (msg.startsWith("TOP")) {
-                if (args.length < 2) {
-                    sock.write("-ERR No message specified\r\n");
-                    return;
-                }
+                if (args.length < 2)
+                    return void sock.write("-ERR No message specified\r\n");
                 const mail = await user.getMail(parseInt(args[0].trim()));
-                if (!mail) {
-                    sock.write("-ERR No such message\r\n");
-                    return;
-                }
+                if (!mail)
+                    return void sock.write("-ERR No such message\r\n");
                 const content = await readFile("mails/" + mail.content + ".txt", "utf-8");
                 const lines = content.split("\r\n");
                 const top = lines.slice(0, 10).join("\r\n");
@@ -106,15 +92,11 @@ export default class POP3Server {
                 sock.write(".\r\n");
             }
             else if (msg.startsWith("DELE")) {
-                if (args.length < 1) {
-                    sock.write("-ERR No message specified\r\n");
-                    return;
-                }
+                if (args.length < 1)
+                    return void sock.write("-ERR No message specified\r\n");
                 const mail = await user.getMail(parseInt(args[0].trim()));
-                if (!mail) {
-                    sock.write("-ERR No such message\r\n");
-                    return;
-                }
+                if (!mail)
+                    return void sock.write("-ERR No such message\r\n");
                 await mail.destroy();
                 sock.write("+OK\r\n");
             }
