b287bdb3527206d22a08cb92b7958900eef0c1de
cfp <44261356+cfpwastaken@users.noreply.github.com>
2023-04-26 20:11:25 +0200

    Update src/config.ts
    Co-authored-by: j0code <42189560+j0code@users.noreply.github.com>

--- src/config.ts
+++ src/config.ts
@@ -6,6 +6,6 @@ export const config = existsSync("config.json") ? JSON.parse(await readFile("con
 export default function getConfig(key: string, defaultValue?: any) {
 	if(config[key]) return config[key]
 	if(process.env[key]) return process.env[key]
-	if(defaultValue) return defaultValue
+	if(defaultValue != undefined) return defaultValue
 	throw new Error("Config key " + key + " not found") // TODO: better error handling?
 }
