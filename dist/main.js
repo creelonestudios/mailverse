e495667e7a69b124ee8a5ec64905a1137fd19769
Cfp <chaosflo44plus@gmail.com>
2023-04-28 17:17:08 +0200

    Add TLS Support to SMTP

--- dist/main.js
+++ dist/main.js
@@ -29,8 +29,10 @@ secure: if (getConfig("enable_pop3s", false) || getConfig("enable_smtps", false)
     }
     if (getConfig("enable_pop3s", false))
         new POP3Server(getConfig("pop3s_port", 995), true, tlsKey, tlsCert); // Port 110 for regular POP3, 995 for POP3S
+    if (getConfig("enable_smtps", false))
+        new SMTPServer(getConfig("smtps_port", 465), true, tlsKey, tlsCert); // Port 25 for regular SMTP, 465 for SMTPS
 }
 if (getConfig("enable_smtp", true))
-    new SMTPServer(getConfig("smtp_port", 25)); // Port 25 for regular SMTP, 465 for SMTPS
+    new SMTPServer(getConfig("smtp_port", 25), false); // Port 25 for regular SMTP, 465 for SMTPS
 if (getConfig("enable_pop3", true))
     new POP3Server(getConfig("pop3_port", 110), false); // Port 110 for regular POP3, 995 for POP3S
