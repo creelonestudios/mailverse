57100d380547d0a76c5a849fa2febeaa67ea754d
j0code <jonaslp759@gmail.com>
2023-04-29 16:24:54 +0200

    tsc build

--- dist/main.js
+++ dist/main.js
@@ -28,4 +28,12 @@ secure: if (getConfig("pop3s.enabled", false) || getConfig("smtps.enabled", fals
     catch (ignore) {
         break secure;
     }
-
+    if (getConfig("pop3s.enabled", false))
+        new POP3Server(getConfig("pop3s.port", 995), true, tlsKey, tlsCert); // Port 110 for regular POP3, 995 for POP3S
+    if (getConfig("smtps.enabled", false))
+        new SMTPServer(getConfig("smtps.port", 465), true, tlsKey, tlsCert); // Port 25 for regular SMTP, 465 for SMTPS
+}
+if (getConfig("smtp.enabled", true))
+    new SMTPServer(getConfig("smtp.port", 25), false); // Port 25 for regular SMTP, 465 for SMTPS
+if (getConfig("pop3.enabled", true))
+    new POP3Server(getConfig("pop3.port", 110), false); // Port 110 for regular POP3, 995 for POP3S
