a2b4213aeda3209973c04c044dff6975ea7b4051
Cfp <chaosflo44plus@gmail.com>
2023-04-29 14:24:51 +0200

    Add spaces in SMTP.ts

--- src/smtp/SMTP.ts
+++ src/smtp/SMTP.ts
@@ -12,9 +12,11 @@ export default class SMTP {
 		await mkdir(`mails/`, { recursive: true })
 		await writeFile(`mails/${id}.txt`, info.content)
 		logger.log(`Saved mail to mails/${id}.txt`)
+
 		const serverName = getConfig("host")
 		if(info.from.endsWith("@" + serverName)) {
 			logger.log("Mail is from this server.")
+
 			if(!info.to.every(email => email.endsWith("@" + serverName))) { // if not all recipients are on this server
 				logger.log("Not all recipients are on this server. Will forward mail to other servers.");
 				logger.error("Forwarding mails to other servers is not implemented yet.")
@@ -25,20 +27,24 @@ export default class SMTP {
 		} else if(!info.to.every(email => email.endsWith("@" + serverName))) {
 			logger.warn("Not all recipients are from this server. Will NOT forward mail to other servers.")
 		}
+
 		const recipients = info.to.filter(email => email.endsWith("@" + serverName))
 		for(const rec of recipients) {
 			logger.log("Forwarding mail to " + rec);
+
 			const user = await User.findOne({ where: { username: rec.split("@")[0] } });
 			if(!user) {
 				logger.error("User " + rec + " does not exist.")
 				// Since we verify the recipients at the RCPT TO command, we should never get here, but you never know
 				continue
 			}
+
 			await user.$create("mail", {
 				from: info.from,
 				to: rec,
 				content: id
 			});
+			
 			logger.log("Forwarded mail to " + rec);
 		}
 	}
